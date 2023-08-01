9f552fcfe5d1b1b4f57bba14735a9f866df55948
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-08-01 05:40:01 +0530

    starte code for Frontend

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./Components/Navbar";
+import Homepage from "./Components/Homepage";
+
+
+
+
+
+function App() {
+  return (
+    
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+           
+          </Routes>
+        </>
+      </Router>
+   
+  );
+}
+
+export default App;
---
+++ client/src/Components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div>
+      This is homepage
+      <div>
+        <img src="" alt="" />
+        
+      </div>
+    </div>
+  )
+}
