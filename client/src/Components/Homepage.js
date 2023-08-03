d5b8530b355fd07239b9d51ea412d556606cf6a8
CherylM <cmuschallik@gmail.com>
2023-08-02 20:58:20 -0400

    Added styles for site background and login. Styling for messages when they populated and added a Messages component.

--- client/src/Components/Homepage.js
+++ client/src/Components/Homepage.js
@@ -2,12 +2,29 @@ import React from 'react'
 
 export default function Homepage() {
   return (
-    <div>
-      This is homepage
-      <div>
-        <img src="" alt="" />
-        
+    <div class="container">
+
+    <div class="container login d-flex justify-content-center">
+      <form>
+      <h2>LOGIN</h2>
+
+        <div class="mb-3">
+
+          <input type="email" class="form-control" id="inputEmail" placeholder="Email address"></input>
+        </div>
+
+        <div class="mb-3">
+
+          <input type="email" class="form-control" id="inputEmail" placeholder="Password"></input>
+
+        </div>
+
+      <button type="submit" class="btn btn-primary">SUBMIT</button>
+
+      </form>
+
       </div>
+
     </div>
   )
 }
