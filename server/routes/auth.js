8d10e185c9cb9ec4a4ad8933e3585994588b4a7b
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-08-10 22:15:38 +0530

    Login route updated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -23,6 +23,21 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      !user && res.status(404).json("user not found");
+  
+      const validPassword = await bcrypt.compare(req.body.password, user.password)
+      !validPassword && res.status(400).json("wrong password")
+  
+      res.status(200).json(user)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  });
+
 
 
 module.exports = router;
