3139963edbb369002532e64be0359c1db3d61d4e
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-08-10 22:25:22 +0530

    like /dislike functionality added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -43,5 +43,54 @@ router.put("/:id", async (req, res) => {
     }
   });
 
+
+
+  //delete a post
+router.delete("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.deleteOne();
+        res.status(200).json("The post has been deleted.");
+      } else {
+        res.status(403).json("You can delete only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  //like / dislike a post
+  
+  router.put("/:id/like", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post.likes.includes(req.body.userId)) {
+        await post.updateOne({ $push: { likes: req.body.userId } });
+        res.status(200).json("The post has been liked");
+      } else {
+        await post.updateOne({ $pull: { likes: req.body.userId } });
+        res.status(200).json("The post has been disliked");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  //get timeline posts
+  
+  router.get("/timeline/:userId", async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.params.userId);
+      const userPosts = await Post.find({ userId: currentUser._id });
+      const friendPosts = await Promise.all(
+        currentUser.followings.map((friendId) => {
+          return Post.find({ userId: friendId });
+        })
+      );
+      res.status(200).json(userPosts.concat(...friendPosts));
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
   module.exports = router;
   
