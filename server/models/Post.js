a2fbb0ac02023c097c007f468ef7fa693cf7745a
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-08-10 22:09:44 +0530

    POSt model UPdated

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,47 +1,24 @@
-const { Schema, model } = require("mongoose");
-const reactions = require('./reaction')
+const mongoose = require("mongoose");
 
-
-// Schema to create Thought model
-const thoughtSchema = new Schema(
+const PostSchema = new mongoose.Schema(
   {
-    thoughtText: {
-      type: String,
-      required: true,
-      minLength: 1,
-      maxLength: 280,
-    },
-    // createdAt: {
-    //   type: Date,
-    //   default: Date.now,
-    //   get: (date) => {
-    //     if (date) return date.toISOString().split("T")[0];
-    //   },
-    // },
-    username: {
+    userId: {
       type: String,
       required: true,
     },
-    reactions: [reactions],
+    desc: {
+      type: String,
+      max: 600,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
   },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-    id: false,
-  }
+  { timestamps: true }
 );
 
-// Created a virtual property `reactionCount` that gets the amount of reactions per user
-
-thoughtSchema
-  .virtual("reactionCount")
-  // Getter
-  .get(function () {
-    return this.reactions.length;
-  });
-
-// Initialize our Thought model
-const Thought = model("thoughts", thoughtSchema);
-
-module.exports = Thought;
+module.exports = mongoose.model("Post", PostSchema);
