55ffcf37f9b4af503ac9d630e82f510ae19bb835
Arun Kaushal <arunkaushal84@gmail.com>
2023-08-14 19:38:37 -0400

    bug fixed

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Homepage from "./Components/Homepage";
 import ProfilePage from "./Components/Profile/profile";
+import SignupForm from "./Components/Signup";
+import LoginForm from "./Components/Login";
 
 // import Messages from "./Components/Messages";
 
@@ -20,6 +22,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/login" element={<LoginForm/>} />
+            <Route path="/signup" element={<SignupForm />} />
           </Routes>
         </>
       </Router>
